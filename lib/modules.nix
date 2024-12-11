eb25feb8695a2bd2820c88b36bded6b655b634f7
Sergey Kazenyuk <kazenyuk@pm.me>
2024-12-11 19:52:48 +0300

    modules: key for the module generated by doRename (renamedOptionModule)

--- lib/modules.nix
+++ lib/modules.nix
@@ -1929,6 +1929,7 @@ let
         opt.type or (types.submodule { });
     in
     {
+      key = "renamedOptionModule#" + concatStringsSep "_" from + "->" + concatStringsSep "_" to;
       options = setAttrByPath from (
         mkOption {
           inherit visible;
